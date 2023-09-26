18de0ac1a4c34b8140ba758042820661025a203a
mustafadrr32 <143702502+mustafadrr32@users.noreply.github.com>
2023-09-26 11:57:30 +0200

    Person bestand gupushed
    ..

---
+++ Feestlunch/speelhal.py
@@ -0,0 +1,3 @@
+totaal = int((9 * 0.37) * 4) + int(7.45 * 4)
+
+print(f"Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {totaal}")
---
+++ module1/deel2/person.py
@@ -0,0 +1,11 @@
+naam = input("Voer uw voor en achternaam in: ")
+adres = input("Voer uw adres in: ")
+postcode = input("Voer uw postcode in: ")
+woonplaats = input("Voer uw woonplaats in: ")
+
+print("----------------------------------------------------")
+print(f"|  Naam      : {naam}   ")
+print(f"|  Adres     : {adres} ")
+print(f"|  Postcode  : {postcode}   ")
+print(f"|  Woonplaats: {woonplaats}")
+print("----------------------------------------------------")
