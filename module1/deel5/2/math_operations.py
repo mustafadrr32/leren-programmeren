bcd59d51c7ad2fb7c3c39b3c22d0e0ad781d56af
mustafadrr32 <143702502+mustafadrr32@users.noreply.github.com>
2023-10-25 09:40:35 +0200

    math_operations

---
+++ module1/deel5/2/math_operations.py
@@ -0,0 +1,23 @@
+# math_operations.py
+
+def increment(number):
+    return number + 1
+
+def decrement(number):
+    return number - 1
+
+def add(number1, number2):
+    return number1 + number2
+
+def substract(number1, number2):
+    return number1 - number2
+
+def multiply(number1, number2):
+    return number1 * number2
+
+def divide(number1, number2):
+    try:
+        result = number1 / number2
+        return result
+    except ZeroDivisionError:
+        return None
