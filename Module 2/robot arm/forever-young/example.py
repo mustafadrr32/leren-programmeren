1dd217f0e638b3ef2fbb6dd2732e5e85651ded67
mustafadrr32 <143702502+mustafadrr32@users.noreply.github.com>
2023-11-21 13:41:56 +0100

    example 1 commit

---
+++ Module 2/robot arm/forever-young/example.py
@@ -0,0 +1,200 @@
+
+# from RobotArm import RobotArm
+
+# robotArm = RobotArm('exercise 3')
+
+# # Jouw python instructies zet je vanaf hier:
+
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.wait()
+
+# <--------------------------------------------------> Oefening 3
+
+
+# from RobotArm import RobotArm
+
+# robotArm = RobotArm('exercise 4')
+
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveRight()
+# robotArm.grab()
+# robotArm.moveLeft()
+# robotArm.drop()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.grab()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.drop()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.grab()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.drop()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.grab()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.drop()
+
+# robotArm.speed = 99999
+
+
+# robotArm.wait()
+
+#<---------------------------------------- Oefening 4 -->
+
+
+# from RobotArm import RobotArm
+
+# robotArm = RobotArm('exercise 5')
+
+# # Jouw python instructies zet je vanaf hier:
+
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.moveRight()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.wait()
+
+# <-------- oefening 5 ------ >
+
+# from RobotArm import RobotArm
+# robotArm = RobotArm('exercise 6')
+
+# robotArm.moveRight()
+# robotArm.grab()
+# robotArm.moveLeft()
+# robotArm.drop()
+# robotArm.moveRight()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveLeft()
+# robotArm.drop()
+# robotArm.moveRight()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+# robotArm.moveLeft()
+# robotArm.grab()
+# robotArm.moveLeft()
+# robotArm.drop()
+# robotArm.moveRight()
+# robotArm.grab()
+# robotArm.moveRight()
+# robotArm.drop()
+
+
+
+# robotArm.wait()
+
+# <<------ oefening 6 --->
+
+
+
+
---
+++ Module 2/robot arm/forever-young/example1.py
@@ -0,0 +1,13 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 1')
+
+# Jouw python instructies zet je vanaf hier:
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.moveRight()
+robotArm.grab()
+robotArm.moveLeft()
+robotArm.drop()
+robotArm.wait()
