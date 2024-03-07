0f10cbcf557995dd987e4b23e642374f5e159260
mustafadrr32 <143702502+mustafadrr32@users.noreply.github.com>
2024-03-07 09:47:59 +0100

    commit 1

--- Module 3/deel 1/rename_me.py
+++ Module 3/deel 1/rename_me.py
@@ -1,16 +1,21 @@
 def quantum_broodrooster(stellar_broccoli:int) -> bool:
     return stellar_broccoli % 2 == 0
 
+#print(quantum_broodrooster(4)) #evengetal 
+
 def chaos_papegaai(fantasie_platypus:str) -> str:
     betoverde_druif = fantasie_platypus.split()
     doldwaze_broccoli = betoverde_druif[::-1]
     tijdmachine_pannenkoekenmix = ' '.join(doldwaze_broccoli)
     return tijdmachine_pannenkoekenmix
 
+#print(chaos_papegaai("hello world")) hij print alles omgekeerde volgorde
+
 def kosmische_koekjesmix(galactische_snoepjes:str) -> int:
     planetair_taartje = set(galactische_snoepjes)
     whatchamacallit = len(planetair_taartje)
     return whatchamacallit
+#print(kosmische_koekjesmix("")) telt het unieke letters.
 
 def ruimte_hamsterwiel(planetair_taartje:str) -> float:
     wobbelwobbel = planetair_taartje.split()
@@ -22,7 +27,12 @@ def ruimte_hamsterwiel(planetair_taartje:str) -> float:
     bizarro_matrix = blork / len(wobbelwobbel)
     return bizarro_matrix
 
+#print(ruimte_hamsterwiel("87")) #hoeveel characters float heeft
+
+
 def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None:
     for zwabber_krakeling in range(1, parallelle_tosti+1):
         laser_sandwich = zwabber_krakeling * infinity_pizza
         print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
+#spaghetti_spaceship(2) Laat de tafel van (getal) zien
+
