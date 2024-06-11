7c30010edf34375de0406f7d55e3769399403404
mustafadrr32 <143702502+mustafadrr32@users.noreply.github.com>
2024-06-11 09:27:30 +0200

    pseudocode en flowchart!

---
+++ Module 3/Proef/ijssalon/pseudocode.py
@@ -0,0 +1,43 @@
+# Start Program
+
+# Print "Welkom bij Papi Gelato je mag alles maken kiezen zolang het maar vanille ijs is."
+
+# Stap 1:
+#     Print "Hoeveel bolletjes wilt u?"
+#     Input aantal_bolletjes
+
+#     If aantal_bolletjes is between 1 and 3:
+#         Go to Stap 2
+#     Else If aantal_bolletjes is between 4 and 8:
+#         Print "Dan krijgt u van mij een bakje met {aantal_bolletjes} bolletjes"
+#         Go to Stap 3
+#     Else If aantal_bolletjes is greater than 8:
+#         Print "Sorry, zulke grote bakken hebben we niet"
+#         Repeat Stap 1
+#     Else:
+#         Print "Sorry, dat snap ik niet..."
+#         Repeat Stap 1
+
+# Stap 2:
+#     Print "Wilt u deze {aantal_bolletjes} bolletje(s) in een hoorntje of een bakje?"
+#     Input keuze
+
+#     If keuze is "hoorntje" or "bakje":
+#         Go to Stap 3
+#     Else:
+#         Print "Sorry, dat snap ik niet..."
+#         Repeat Stap 2
+
+# Stap 3:
+#     Print "Hier is uw {hoorntje/bakje} met {aantal_bolletjes} bolletje(s)."
+#     Print "Wilt u nog meer bestellen?"
+#     Input meer_bestellen
+
+#     If meer_bestellen is "ja":
+#         Go to Stap 1
+#     Else If meer_bestellen is "nee":
+#         Print "Bedankt en tot ziens!"
+#         Stop Program
+#     Else:
+#         Print "Sorry, dat snap ik niet..."
+#         Repeat Stap 3
